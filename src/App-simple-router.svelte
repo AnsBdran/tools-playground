7822e5e6afbc8c18710fa930a7489199649e1cce
Anas Badran <ansaccts@gmail.com>
2025-06-05 16:22:54 +0300

    routes switching scrolling bug => temp fix

--- src/App-simple-router.svelte
+++ src/App-simple-router.svelte
@@ -15,6 +15,7 @@
 	import Home from './routes/home.svelte';
 	import About from './routes/about.svelte';
 	import Login from './routes/login.svelte';
+	import { AppState } from './App.state.svelte';
 	import { RenderScan } from 'svelte-render-scan';
 	import { scrollY } from 'svelte/reactivity/window';
 	import { watch } from 'runed';
@@ -46,36 +47,39 @@
 		}
 	];
 
+	const appState = new AppState();
+
 	let routesYScroll: Record<string, number> = $state({});
 	const options: RouterOptions<RouteNames> = {
 		routes,
-		onStart: (thing) => {
-			console.log('beforeEach ' + scrollY.current, routesYScroll, thing);
-			if (!thing.from.location) return;
-			routesYScroll[thing.from.location.path] = scrollY.current ?? 0;
-		},
+		// onStart: (thing) => {
+		// 	// console.log('beforeEach ' + scrollY.current, routesYScroll, thing);
+		// 	if (!thing.from.location) return;
+		// 	if (routesYScroll[thing.from.location.path]) return;
+		// 	routesYScroll[thing.from.location.path] = scrollY.current ?? 0;
+		// },
 		onEnd: (thing) => {
-			console.log('onEnd', routesYScroll);
+			// console.log('onEnd', routesYScroll);
 			if (
-				!routesYScroll[thing.to.route.path] ||
+				!appState.scroll[thing.to.route.path] ||
 				thing.to.route.path === thing.from.location?.path
 			) {
-				console.log('same path');
+				// console.log('same path');
 				return window.scrollTo({ top: 0, behavior: 'instant' });
 			}
-			console.log('scrollTo found old value', routesYScroll[thing.to.route.path]);
-			window.scrollTo({ top: routesYScroll[thing.to.route.path], behavior: 'instant' });
-		},
-		logLevel: 3
+			// console.log('scrollTo found old value', routesYScroll[thing.to.route.path]);
+			window.scrollTo({ top: appState.scroll[thing.to.route.path], behavior: 'instant' });
+		}
+		// logLevel: 3
 	} as const;
 	onDestroy(() => {});
 	watch(
-		() => null,
+		() => scrollY.current,
 		() => {
-			console.log('#'.repeat(30));
-			console.log('#'.repeat(30));
+			appState.scroll[window.location.pathname] = scrollY.current ?? 0;
 		}
 	);
+	$inspect(routesYScroll);
 </script>
 
 <div class=" p-4 pt-16 flex flex-col gap-24f w-full">
